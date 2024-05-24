76d8e08e91a060a96760924c13662c386930e44f
Surakit <surakitkunkayun@gmail.com>
2024-05-24 13:34:33 +0700

    feat: finished assignment

--- ex-1.js
+++ ex-1.js
@@ -11,6 +11,8 @@ let additionalProperties = {
   hobbies: ["Football", "Coding"],
 };
 
-// Start coding here
+let newStudentProfile = {...studentProfile, ...additionalProperties};
 
 console.log(newStudentProfile);
+
+ 
